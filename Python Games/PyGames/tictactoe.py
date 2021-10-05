5c234fd857da18035ca5cbea33bd1eb29529cc84
hritikasaxena <hritikasaxena217@gmail.com>
2021-10-06 00:42:43 +0530

    tictactoe

--- Python Games/PyGames/tictactoe.py
+++ Python Games/PyGames/tictactoe.py
@@ -1,41 +1,263 @@
-import tkinter as tk
+from tkinter import *
+from tkinter import messagebox
+
+count = 0
+board = [
+    [
+        "",
+        "",
+        "",
+    ],
+    [
+        "",
+        "",
+        "",
+    ],
+    [
+        "",
+        "",
+        "",
+    ],
+]
 
 
-def fahrenheit_to_celsius():
-    """Convert the value for Fahrenheit to Celsius and insert the
-    result into lbl_result.
-    """
-    fahrenheit = ent_temperature.get()
-    celsius = (5 / 9) * (float(fahrenheit) - 32)
-    lbl_result["text"] = f"{round(celsius, 2)} \N{DEGREE CELSIUS}"
+# Whenever the user selects the quit option, this message is displayed
+def Quit():
+    global t
+    msg = messagebox.askquestion(
+        "Confirm", "Are you want to Quit? You still have chances!"
+    )
+    if msg == "yes":
+        t.destroy()
 
 
-# Set-up the window
-window = tk.Tk()
-window.title("Temperature Converter")
-window.resizable(width=False, height=False)
+# Destructs the winner window and game window
+def destruct():
+    global t, winnerWindow
+    t.destroy()
+    winnerWindow.destroy()
 
-# Create the Fahrenheit entry frame with an Entry
-# widget and label in it
-frm_entry = tk.Frame(master=window)
-ent_temperature = tk.Entry(master=frm_entry, width=10)
-lbl_temp = tk.Label(master=frm_entry, text="\N{DEGREE FAHRENHEIT}")
 
-# Layout the temperature Entry and Label in frm_entry
-# using the .grid() geometry manager
-ent_temperature.grid(row=0, column=0, sticky="e")
-lbl_temp.grid(row=0, column=1, sticky="w")
+# Displays the winning condition
+def displayWinner(winner):
+    global t, winnerWindow, ID
+    winnerWindow = Tk()
+    winnerWindow.title("Winner Window")
+    winnerWindow.configure(bg="Black")
+    l1 = Label(
+        winnerWindow,
+        text="THE WINNER IS: ",
+        font=("COMIC SANS MS", 15),
+        bg="Black",
+        fg="White",
+    )
+    l1.pack()
+    l2 = Label(
+        winnerWindow, text=winner, font=("COMIC SANS MS", 15), bg="Black", fg="White"
+    )
+    l2.pack()
+    bproceed = Button(
+        winnerWindow,
+        text="Proceed",
+        font=("COMIC SANS MS", 10, "bold"),
+        command=destruct,
+    )
+    bproceed.pack()
 
-# Create the conversion Button and result display Label
-btn_convert = tk.Button(
-    master=window, text="\N{RIGHTWARDS BLACK ARROW}", command=fahrenheit_to_celsius
-)
-lbl_result = tk.Label(master=window, text="\N{DEGREE CELSIUS}")
 
-# Set-up the layout using the .grid() geometry manager
-frm_entry.grid(row=0, column=0, padx=10)
-btn_convert.grid(row=0, column=1, pady=10)
-lbl_result.grid(row=0, column=2, padx=10)
+# Checks for the winner
+def checkWinner():
+    global count, board
+    if (
+        board[0][0] == board[0][1] == board[0][2] == "X"
+        or board[1][0] == board[1][1] == board[1][2] == "X"
+        or board[2][0] == board[2][1] == board[2][2] == "X"
+        or board[0][0] == board[1][0] == board[2][0] == "X"
+        or board[0][1] == board[1][1] == board[2][1] == "X"
+        or board[0][2] == board[1][2] == board[2][2] == "X"
+        or board[0][0] == board[1][1] == board[2][2] == "X"
+        or board[0][2] == board[1][1] == board[2][0] == "X"
+    ):
+        displayWinner("Player X")
+    elif (
+        board[0][0] == board[0][1] == board[0][2] == "O"
+        or board[1][0] == board[1][1] == board[1][2] == "O"
+        or board[2][0] == board[2][1] == board[2][2] == "O"
+        or board[0][0] == board[1][0] == board[2][0] == "O"
+        or board[0][1] == board[1][1] == board[2][1] == "O"
+        or board[0][2] == board[1][2] == board[2][2] == "O"
+        or board[0][0] == board[1][1] == board[2][2] == "O"
+        or board[0][2] == board[1][1] == board[2][0] == "O"
+    ):
+        displayWinner("Player O")
+    elif count == 9:
+        displayWinner("NONE! IT IS A TIE!")
 
-# Run the application
-window.mainloop()
+
+# Changes the value of the button
+def changeVal(button, boardValRow, boardValCol):
+    global count
+
+    # Checking if button is available
+    if button["text"] == "":
+        if count % 2 == 0:
+            button["text"] = "X"
+            l1 = Label(
+                t,
+                text="PLAYER: 2(O)",
+                height=3,
+                font=("COMIC SANS MS", 10, "bold"),
+                bg="white",
+            ).grid(row=0, column=0)
+            board[boardValRow][boardValCol] = "X"
+        else:
+            button["text"] = "O"
+            l1 = Label(
+                t,
+                text="PLAYER: 1(X)",
+                height=3,
+                font=("COMIC SANS MS", 10, "bold"),
+                bg="white",
+            ).grid(row=0, column=0)
+            board[boardValRow][boardValCol] = "O"
+        count = count + 1
+        if count >= 5:
+            checkWinner()
+    else:
+        messagebox.showerror("Error", "This box already has a value!")
+
+
+# Displays the GUI
+def TicTacToeGUI():
+    global t
+    t = Tk()
+    t.title("TIC TAC TOE")
+    t.configure(bg="white")  # Making the background of the window as white
+
+    # Displaying the player
+    l1 = Label(
+        t, text="PLAYER: 1(X)", height=3, font=("COMIC SANS MS", 10, "bold"), bg="white"
+    )
+    l1.grid(row=0, column=0)
+
+    # Quit button
+    exitButton = Button(
+        t, text="Quit", command=Quit, font=("COMIC SANS MS", 10, "bold")
+    )
+    exitButton.grid(row=0, column=2)
+
+    # Grid buttons
+    b1 = Button(
+        t,
+        text="",
+        height=4,
+        width=8,
+        bg="black",
+        activebackground="white",
+        fg="white",
+        font="Times 15 bold",
+        command=lambda: changeVal(b1, 0, 0),
+    )
+    b2 = Button(
+        t,
+        text="",
+        height=4,
+        width=8,
+        bg="black",
+        activebackground="white",
+        fg="white",
+        font="Times 15 bold",
+        command=lambda: changeVal(b2, 0, 1),
+    )
+    b3 = Button(
+        t,
+        text="",
+        height=4,
+        width=8,
+        bg="black",
+        activebackground="white",
+        fg="white",
+        font="Times 15 bold",
+        command=lambda: changeVal(b3, 0, 2),
+    )
+    b4 = Button(
+        t,
+        text="",
+        height=4,
+        width=8,
+        bg="black",
+        activebackground="white",
+        fg="white",
+        font="Times 15 bold",
+        command=lambda: changeVal(b4, 1, 0),
+    )
+    b5 = Button(
+        t,
+        text="",
+        height=4,
+        width=8,
+        bg="black",
+        activebackground="white",
+        fg="white",
+        font="Times 15 bold",
+        command=lambda: changeVal(b5, 1, 1),
+    )
+    b6 = Button(
+        t,
+        text="",
+        height=4,
+        width=8,
+        bg="black",
+        activebackground="white",
+        fg="white",
+        font="Times 15 bold",
+        command=lambda: changeVal(b6, 1, 2),
+    )
+    b7 = Button(
+        t,
+        text="",
+        height=4,
+        width=8,
+        bg="black",
+        activebackground="white",
+        fg="white",
+        font="Times 15 bold",
+        command=lambda: changeVal(b7, 2, 0),
+    )
+    b8 = Button(
+        t,
+        text="",
+        height=4,
+        width=8,
+        bg="black",
+        activebackground="white",
+        fg="white",
+        font="Times 15 bold",
+        command=lambda: changeVal(b8, 2, 1),
+    )
+    b9 = Button(
+        t,
+        text="",
+        height=4,
+        width=8,
+        bg="black",
+        activebackground="white",
+        fg="white",
+        font="Times 15 bold",
+        command=lambda: changeVal(b9, 2, 2),
+    )
+
+    b1.grid(row=2, column=0)
+    b2.grid(row=2, column=1)
+    b3.grid(row=2, column=2)
+
+    b4.grid(row=3, column=0)
+    b5.grid(row=3, column=1)
+    b6.grid(row=3, column=2)
+
+    b7.grid(row=4, column=0)
+    b8.grid(row=4, column=1)
+    b9.grid(row=4, column=2)
+
+
+TicTacToeGUI()
